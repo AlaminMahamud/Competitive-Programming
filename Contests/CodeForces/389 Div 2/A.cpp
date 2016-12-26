3c79855fb0e974331f64344156fe8abf3f592f05
AlaminMahamud <alamin.ineedahelp@gmail.com>
2016-12-26 11:58:28 +0600

    Updating while init

---
+++ Contests/CodeForces/389 Div 2/A.cpp
@@ -0,0 +1,444 @@
+/**********************************************************************************************************
+Look, I was gonna go easy on you not to hurt your feelings
+But I'm only going to get this one chance
+(Six minutes, six minutes)
+Something's wrong, I can feel it
+(Six minutes, six minutes, Slim Shady, you're on)
+Just a feeling I've got
+Like something's about to happen
+But I don't know what
+If that means, what I think it means, we're in trouble
+Big trouble. And if he is as bananas as you say
+I'm not taking any chances
+You were just what the doctor ordered
+
+
+
+
+I'm beginning to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+Now who thinks their arms are long enough to slap box, slap box?
+They said I rap like a robot, so call me rap-bot
+
+
+
+
+But for me to rap like a computer must be in my genes
+I got a laptop in my back pocket
+My pen'll go off when I half-cock it
+Got a fat knot from that rap profit
+Made a living and a killing off it
+Ever since Bill Clinton was still in office
+With Monica Lewinski feeling on his nutsack
+I'm an MC still as honest
+But as rude and as indecent as all hell
+Syllables, skill-a-holic (Kill 'em all with)
+This flippity, dippity-hippity hip-hop
+You don't really wanna get into a pissing match
+With this rappity-rap
+Packing a mack in the back of the Ac
+backpack rap, crap, yap-yap, yackety-yack
+and at the exact same time
+I attempt these lyrical acrobat stunts while I'm practicing that
+I'll still be able to break a motherfuckin' table
+Over the back of a couple of faggots and crack it in half
+Only realized it was ironic
+I was signed to Aftermath after the fact
+How could I not blow? All I do is drop "F" bombs
+Feel my wrath of attack
+Rappers are having a rough time period
+Here's a Maxi-Pad
+It's actually disastrously bad
+For the wack while I'm masterfully constructing this masterpiece yeah
+
+
+
+'Cause I'm beginning to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+Now who thinks their arms are long enough to slap box, slap box?
+Let me show you maintaining this shit ain't that hard, that hard
+
+
+
+Everybody want the key and the secret to rap
+Immortality like I have got
+Well, to be truthful the blueprint's
+Simply rage and youthful exuberance
+Everybody loves to root for a nuisance
+Hit the earth like an asteroid
+and did nothing but shoot for the moon since (PPEEYOOM)
+MC's get taken to school with this music
+'Cause I use it as a vehicle to 'bus the rhyme'
+Now I lead a New School full of students
+Me? Me, I'm a product of Rakim
+Lakim Shabazz, 2Pac, N-W-A., Cube, hey, Doc, Ren
+Yella, Eazy, thank you, they got Slim
+Inspired enough to one day grow up
+Blow up and being in a position
+To meet Run-D.M.C. and induct them
+Into the motherfuckin' Rock n'
+Roll Hall of Fame even though I walk in the church
+And burst in a ball of flames
+Only Hall of Fame I'll be inducted in is the alcohol of fame
+On the wall of shame
+You fags think it's all a game
+'Til I walk a flock of flames
+Off a plank and
+Tell me what in the fuck are you thinking?
+Little gay looking boy
+So gay I can barely say it with a 'straight' face looking boy
+You're witnessing a mass-occur like you're watching a church gathering
+And take place looking boy
+Oy vey, that boy's gay
+That's all they say looking boy
+You get a thumbs up, pat on the back
+And a "way to go" from your label every day looking boy
+Hey, looking boy, what d'you say looking boy?
+I get a "hell yeah" from Dre looking boy
+I'mma work for everything I have
+Never asked nobody for shit
+Git out my face looking boy
+Basically boy you're never gonna be capable
+of keeping up with the same pace looking boy, 'cause
+
+
+
+I'm beginning to feel like a Rap God, Rap God
+All my people from the front to the back nod, back nod
+The way I'm racing around the track, call me Nascar, Nascar
+Dale Earnhardt of the trailer park, the White Trash God
+Kneel before General Zod this planet's Krypton, no Asgard, Asgard
+
+
+
+So you'll be Thor and I'll be Odin
+You rodent, I'm omnipotent
+Let off then I'm reloading
+Immediately with these bombs I'm totin'
+And I should not be woken
+I'm the walking dead
+But I'm just a talking head, a zombie floating
+But I got your mom deep throating
+I'm out my Ramen Noodle
+We have nothing in common, poodle
+I'm a Doberman, pinch yourself
+In the arm and pay homage, pupil
+It's me
+My honesty's brutal
+But it's honestly futile if I don't utilize
+What I do though for good
+At least once in a while so I wanna make sure
+Somewhere in this chicken scratch I scribble and doodle
+Enough rhymes to
+Maybe try to help get some people through tough times
+But I gotta keep a few punchlines
+Just in case 'cause even you unsigned
+Rappers are hungry looking at me like it's lunchtime
+I know there was a time where once I
+Was king of the underground
+But I still rap like I'm on my Pharoahe Monch grind
+So I crunch rhymes
+But sometimes when you combine
+Appeal with the skin color of mine
+You get too big and here they come trying to
+Censor you like that one line I said
+On "I'm Back" from the Mathers LP
+One when I tried to say I'll take seven kids from Columbine
+Put 'em all in a line
+Add an AK-47, a revolver and a nine
+See if I get away with it now
+That I ain't as big as I was, but I'm
+Morphin' into an immortal coming through the portal
+You're stuck in a time warp from two thousand four though
+And I don't know what the fuck that you rhyme for
+You're pointless as Rapunzel
+With fucking cornrows
+You write normal, fuck being normal
+And I just bought a new ray gun from the future
+Just to come and shoot ya
+Like when Fabulous made Ray J mad
+'Cause Fab said he looked like a fag
+At Mayweather's pad singin' to a man
+While he play piano
+Man, oh man, that was the 24/7 special
+On the cable channel
+So Ray J went straight to radio station the very next day
+"Hey, Fab, I'mma kill you"
+Lyrics coming at you at supersonic speed, (JJ Fad)
+Uh, summa lumma dooma lumma you assuming I'm a human
+What I gotta do to get it through to you I'm superhuman
+Innovative and I'm made of rubber, so that anything you say is
+Ricochet in off a me and it'll glue to you
+And I'm devastating more than ever demonstrating
+How to give a motherfuckin' audience a feeling like it's levitating
+Never fading, and I know that haters are forever waiting
+For the day that they can say I fell off, they'll be celebrating
+'Cause I know the way to get 'em motivated
+I make elevating music
+You make elevator music
+"Oh, he's too mainstream."
+Well, that's what they do
+When they get jealous, they confuse it
+"It's not hip hop, it's pop."
+'Cause I found a hella way to fuse it
+With rock, shock rap with Doc
+Throw on "Lose Yourself" and make 'em lose it
+I don't know how to make songs like that
+I don't know what words to use
+Let me know when it occurs to you
+While I'm ripping any one of these verses that versus you
+It's curtains, I'm inadvertently hurtin' you
+How many verses I gotta murder to
+Prove that if you were half as nice,
+your songs you could sacrifice virgins to
+Unghh, school flunky, pill junky
+But look at the accolades these skills brung me
+Full of myself, but still hungry
+I bully myself 'cause I make me do what I put my mind to
+When I'm a million leagues above you
+Ill when I speak in tongues
+But it's still tongue-and-cheek, fuck you
+I'm drunk so Satan take the fucking wheel
+I'm asleep in the front seat
+Bumping Heavy D and the Boys
+"Still chunky, but funky"
+But in my head there's something
+I can feel tugging and struggling
+Angels fight with devils and
+Here's what they want from me
+They're asking me to eliminate some of the women hate
+But if you take into consideration the bitter hatred I had
+Then you may be a little patient and more sympathetic to the situation
+And understand the discrimination
+But fuck it
+Life's handing you lemons
+Make lemonade then
+But if I can't batter the women
+How the fuck am I supposed to bake them a cake then?
+Don't mistake him for Satan
+It's a fatal mistake if you think I need to be overseas
+And take a vacation to trip a broad
+And make her fall on her face and
+Don't be a retard, be a king?
+Think not
+Why be a king when you can be a God?
+
+**********************************************************************************************************/
+
+
+/*
+** Author------: Md. Alamin Mahamud
+** Handle------: A1am1N
+** Institution-: CUET
+** Sigil-------: I Ask 'WHY' Rather Than 'WHAT' and 'HOW'
+**-------------: I Just Hate Unfinished Businesses
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+
+// Define file I/O
+#define READ(filename) freopen(filename, "r", stdin);
+#define WRITE(filename) freopen(filename, "w", stdout);
+
+// Define memory set function
+#define MEM(x,y) memset(x,y,sizeof(x))
+#define CLEAR(x) memset(x,0,sizeof(x))
+
+// Define Input
+#define SF scanf
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+
+// Define Output
+#define PF printf
+#define PRD(t) printf("%d",t)
+#define PRLD(t) printf("%ld",t)
+#define PRLLD(t) printf("%lld",t)
+#define PRC(t) printf("%c",t)
+#define PRS(t) printf("%s",t)
+#define PRF(t) printf("%f",t)
+#define PRLF(t) printf("%lf",t)
+#define PRN() printf("\n")
+
+// Define Fucntions And Objects
+#define pb push_back
+#define SORT(v) sort(v.begin(),v.end())
+#define RSORT(v) sort(v.rbegin(),v.rend())
+#define CSORT(v,c) sort(v.begin(),v.end(),c)
+#define ALL(v) (v).begin(),(v).end()
+#define SQR(x) ((x)*(x))
+#define SZ(x) (return (signed int)x.size())
+#define DIST(a,b) sqrt(sqr(a.x-b.x) + sqr(a.y-b.y))
+
+// Define CONST Value
+#define SIZE 1e9
+#define MIN INT_MIN
+#define MAX INT_MAX
+#define INF 0x7FFFFFFF // check
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define PI2 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+// Define Bitwise Operation
+#define CHECK(n, pos) (n & (1<<(pos)))
+#define BITON(n, pos) (n | (1<<(pos)))
+#define BITOFF(n, pos) (n & ~(1<<(pos)))
+
+// Define LOOP Shortcuts
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+
+// Define Color
+enum {WHITE,GREY,BLACK};
+
+// Sync off with stdio
+#define __ cin.sync_with_stdio(false);\
+        cin.tie();
+
+// Debug tools
+#define what_is(x) cerr<<(#x)<<" is "<<x<<endl
+
+// Typedef's
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef vector<char> VC;
+typedef vector<VC> VC2D;
+typedef map<int,int> MPII;
+typedef map<string,int> MPSI;
+typedef map<int,string> MPIS;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef queue<int> QI;
+typedef queue<QI> QII;
+typedef stack<int> SI;
+typedef stack<SI> SII;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+
+// Template And Structures
+
+// Point for x,y (int) coordinate in 2D space
+struct POINT{
+    int x,y;
+    POINT(){}
+    POINT(int a,int b){x=a,y=b;}
+    bool operator==(const POINT &a)const{return x==a.x and y==a.y;}
+};
+
+// Point for x,y (double) coordinate in 2D space
+struct POINT_DOUBLE{
+    double x,y;
+    POINT_DOUBLE(){}
+    POINT_DOUBLE(double a,double b){x=a,y=b;}
+};
+
+// Node for Dijkstra
+struct Node{
+    int v,w;
+    Node() {}
+    bool operator<(const Node &a)const{return w>a.w;}
+    Node(int _v,int _w){v=_v,w=_w;}
+};
+
+namespace my{
+    template<class T>T gcd(T a,T b){return b == 0 ? a : gcd(b, a % b);}
+
+    template<typename T>T lcm(T a, T b) {return a / gcd(a,b) * b;}
+
+    template<class T>T big_mod(T n,T p,T m){if(p==0)return (T)1;T x=big_mod(n,p/2,m);x=(x*x)%m;if(p&1)x=(x*n)%m;return x;}
+
+    template<class T>T multiplication(T n,T p,T m){if(p==0)return (T)0;T x=multiplication(n,p/2,m);x=(x+x)%m;if(p&1)x=(x+n)%m;return x;}
+
+    template<class T>T my_pow(T n,T p){if(p==0)return 1;T x=my_pow(n,p/2);x=(x*x);if(p&1)x=(x*n);return x;} ///n to the power p
+
+    template <class T> double getdist(T a, T b){return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));}/// distance between a & b
+
+    template <class T> T extract(string s, T ret) {stringstream ss(s); ss >> ret; return ret;}/// extract words or numbers from a line
+
+    template <class T> string tostring(T n) {stringstream ss; ss << n; return ss.str();}/// convert a number to string
+
+    template<class T> inline T Mod(T n,T m) {return (n%m+m)%m;} ///For Positive Negative No.
+
+    template<class T> T MIN3(T a,T b,T c) {return min(a,min(b,c));} /// minimum of 3 number
+
+    template<class T> T MAX3(T a,T b,T c) {return max(a,max(b,c));} ///maximum of 3 number
+
+    template <class T> void print_vector(T &v){int sz=v.size();if(sz)cout<<v[0];for(int i = 1; i < sz; i++)cout << ' '<<v[i];cout<<"\n";}/// prints all elements in a vector
+
+    bool isVowel(char ch){ ch=toupper(ch); if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E') return true; return false;}
+
+    bool isConsonant(char ch){if (isalpha(ch) && !isVowel(ch)) return true; return false;}
+}
+
+namespace debug{
+    int sum(){return 0;}
+    template<typename T,typename... Args> T sum(T a,Args... args) {return a+sum(args...);}
+    void print(){cout<<"\n";return;}template<typename T, typename... Args>void print(T a,Args... args){cout<<a<<" ";print(args...);}
+}
+
+
+// Direction
+// int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1}; ///8 Direction
+// int dx[4] = {1, 0, -1, 0};int dy[4] = {0, 1, 0, -1}; ///4 Direction
+// int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};///Knight Direction
+// int dx[]={-1,-1,+0,+1,+1,+0};int dy[]={-1,+1,+2,+1,-1,-2}; ///Hexagonal Direction
+
+/*******  End All Required define Pre-Processors and typedef Constants *******/
+
+class Solution{
+public:
+    void solve(istream& in, ostream& out);
+};
+
+void Solution :: solve(istream& cin, ostream& cout){
+    int n,m,k;
+    cin >> n >> m >> k;
+    int sum = 0;
+    REP(i,n){
+        sum+=m*2;
+        if(sum >= k) {cout << i+1 << " ";break;}
+    }
+    sum = 0;
+    REP(j,n){
+        FOR(i,0,2*m+1,1){
+            if(2*m*j + i == k) {
+                cout << floor((i+1)/2) << " "; break;
+            }
+        }
+    }
+
+    if(k%2==0)
+    cout << "R" << endl;
+    else cout << "L" << endl;
+
+}
+
+/********** Main()  function **********/
+int main(){
+
+    //READ("in.txt");
+    //WRITE("out.txt");
+    Solution solution;
+    solution.solve(cin,cout);
+    return 0;
+}
+/********  Main() Ends Here *************/
